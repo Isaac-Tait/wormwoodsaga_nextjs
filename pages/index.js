de8d21005b83d5e6a4bbfa51e4c9e638e37729c6
Isaac Tait <isaac@mountaintopcoding.com>
2022-08-31 20:27:43 -0400

    chore: re-work index page 🦊

--- pages/index.js
+++ pages/index.js
@@ -6,34 +6,6 @@ import { getPosts } from '../lib/posts';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Index = (props) => (
-  <div className="heropattern-leaf-neutral-100 h-screen">
-    <Script
-      src="https://unpkg.com/@tryghost/portal@latest/umd/portal.min.js"
-      data-ghost="https://wormwood-saga.ghost.io/"
-    />
-    <div>
-      <Header />
-    </div>
-    <div className="max-w-6xl mx-auto bg-slate-200 rounded-xl">
-      <ul className="pl-2">
-        {props.posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.slug}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-    <div className="fixed bottom-0 w-full">
-      <Footer />
-    </div>
-  </div>
-);
-
-export default Index;
-
 export async function getStaticProps() {
   const posts = await getPosts();
 
@@ -46,4 +18,33 @@ export async function getStaticProps() {
   return {
     props: { posts },
   };
+}
+
+export default function Home({ posts }) {
+  return (
+    <div className="heropattern-leaf-neutral-100 h-screen">
+      <Script
+        src="https://unpkg.com/@tryghost/portal@latest/umd/portal.min.js"
+        data-ghost="https://wormwood-saga.ghost.io/"
+      />
+      <div>
+        <Header />
+      </div>
+
+      <div className="max-w-6xl mx-auto bg-slate-200 rounded-xl">
+        <ul>
+          {posts.map((post) => (
+            <li key={post.uuid}>
+              <Link href={`/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
+    </div>
+  );
 }
