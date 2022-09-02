59f6fba0fc390b8df5bd47eafc1254cbda6b7af0
Isaac Tait <isaac@mountaintopcoding.com>
2022-09-02 19:27:28 -0400

    chore: revert to old code

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,28 +4,16 @@ import { getPosts, getSinglePost } from '../../lib/posts';
 
 import Footer from '../../components/Footer';
 
-export async function getStaticPaths() {
-  const posts = await getPosts();
-  const paths = posts.map(({ slug }) => ({ params: { slug } }));
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-  const { slug } = params;
-  const data = await getSinglePost(slug);
-  return { props: { data } };
-}
-
-export default function Post({ data }) {
+const NovelPage = (props) => {
   return (
     <div className="heropattern-leaf-neutral-100 h-screen overflow-y-scroll">
       <Link href="/">
         <a className="text-3xl font-semibold">Wormwood Saga</a>
       </Link>
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-2xl font-medium">{data.post.title}</h1>
+        <h1 className="text-2xl font-medium">{props.post.title}</h1>
         <div
-          dangerouslySetInnerHTML={{ __html: data.post.html }}
+          dangerouslySetInnerHTML={{ __html: props.post.html }}
           className="mb-12 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-slate-900 first-letter:mr-3 first-letter:float-left"
         />
       </div>
@@ -34,4 +22,35 @@ export default function Post({ data }) {
       </div>
     </div>
   );
+};
+
+export default NovelPage;
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+
+  // Get the paths we want to create based on posts
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  // "fallback: false" gives us a 404 if post not found
+  return { paths, fallback: false };
+}
+
+// Pass the page slug to "getSinglePost()" function
+// Which then passes it to "posts.read()" to query the GhostContentAPI
+export async function getStaticProps(context) {
+  const post = await getSinglePost(context.params.slug);
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { post },
+    revalidate: 1,
+  };
 }
